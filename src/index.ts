5d60c716a521c0fdcf48804ab16d8968d651c3d4
Jonathan Spira <jjspira@gmail.com>
2023-09-03 14:09:52 -0400

    removed axios functionality

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,6 @@ import * as resourceCommand from './resource';
 import * as serializationCommand from './serialization';
 import * as startupOutput from './startup';
 import * as vfsCommand from './vfs';
-import { YyBoss } from './yy_boss';
-import * as fetch from './fetch';
 import * as events from './events';
 
 export {
@@ -28,10 +26,8 @@ export {
     serializationCommand,
     startupOutput,
     vfsCommand,
-    fetch,
     events,
     // simple re-exports...
-    YyBoss,
     ViewPath,
     Resource,
     SerializedDataDefault,
@@ -41,21 +37,3 @@ export {
     CommandOutputSuccessVoid,
     Item,
 };
-
-/**
- * Creates a YyBoss, or throws an error on startup.
- * Use this as a convenient startup method, or invoke the YyBoss directly with `YyBoss.create`.
- */
-export async function quickstartYyBoss(
-    yyBossExePath: string,
-    yypPath: string,
-    workingDirectory: string
-): Promise<YyBoss> {
-    let [output, maybe_boss] = await YyBoss.create(yyBossExePath, yypPath, workingDirectory);
-
-    if (output.success == false) {
-        throw output;
-    }
-
-    return maybe_boss as YyBoss;
-}
